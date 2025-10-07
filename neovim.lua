a655fc5c51ab111220fa7d6c89f87d54a7992a58
Francisco Vargas <haripako@haripako.net>
2025-10-08 00:32:14 +0200

    Add files via upload

---
+++ neovim.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"maxmx03/solarized.nvim",
+		priority = 1000,
+		opts = {
+			variant = "spring",
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "solarized",
+		},
+	},
+}
